73aca4076d5d59632bb9dd084c1b7914dbab3fff
Julian David <julian.mora@crehana.com>
2024-02-14 11:06:37 -0500

    Fix render prod (#2)
    * fix production build & render
    
    * install sharp (recommended by vercel)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,31 @@
 import '~/styles/global.css'
 
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import { lazy } from 'react'
-
 
 export interface SharedPageProps {
   draftMode: boolean
   token: string
 }
 
-const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
+const PreviewProvider = dynamic(() => import('~/components/PreviewProvider'))
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<SharedPageProps>) {
   const { draftMode, token } = pageProps
+
   return (
     <NextUIProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Nigma LandingPage" />
         <title>nigmauniverse.com</title>
-        <html lang="es" />
       </Head>
 
-      <style jsx global>
-        {``}
-      </style>
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
